35ea9b1ff03710fa8b9836206fe6b90e14876ae8
AndrewPlatov <andrey.pshenichny1998@gmail.com>
2025-02-18 21:27:23 +0300

    Сделал подсчет возраста, но есть нюанс...

--- 18 February 2025/FORMS/script.js
+++ 18 February 2025/FORMS/script.js
@@ -5,3 +5,17 @@ function colorize() {
         anq.style.backgroundColor = "#90ABFF"
     }
 }
+
+// Ищем возраст:
+function calculateAge() {
+    const birthInput = document.getElementById('birth');
+    const birth = new Date(birthInput.value);
+    const today = new Date();
+
+    const age = today.getFullYear() - birth.getFullYear();
+
+    alert("Ваш возраст: " + age + " лет");
+}
+
+// Только есть проблема: при вводе с клавиатуры программа реагирует только на первую цифру
+// и сразу выдает возраст. При вводе 1 сразу пишет "возраст 2024", т.е. не дает ввести 1998
