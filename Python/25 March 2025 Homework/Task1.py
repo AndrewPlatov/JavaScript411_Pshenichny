60db9cd04d9b0206d09667de3d9d449891f7a294
AndrewPlatov <andrey.pshenichny1998@gmail.com>
2025-03-26 00:31:25 +0400

    Понял в чем суть, доработано

--- Python/25 March 2025 Homework/Task1.py
+++ Python/25 March 2025 Homework/Task1.py
@@ -1,22 +1,25 @@
 # Task 1
 # Написать свой класс для хранения информации о человеке: фамилия, имя, отчество, дата рождения
 
+from datetime import datetime
+
 class MenInfo:
-    name = "Андрей"
-    surname = "Пшеничный"
-    pathronymic = "Михайлович"
-    year = 1998
-    month = 3
-    day = 2
-    hour = 14
-    minute = 0
-    second = 0
+    def __init__(self, name, surname, patronymic, year, height):
+        self.name = name
+        self.surname = surname
+        self.patronymic = patronymic
+        self.year = year
+        self.height = height
     
-mi = MenInfo()
+    def calculate_age(self):
+        current_year = datetime.now().year
+        return current_year - self.year
+
+
+mi = MenInfo("Андрей", "Пшеничный", "Михайлович", 1998, 171)
+
 print(mi.name)
 print(mi.surname)
-print(mi.pathronymic)
+print(mi.patronymic)
 print(mi.year)
-print(mi.hour)
-print(mi.minute)
-print(mi.second)
+print(mi.calculate_age())
